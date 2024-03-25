2d09e7d56242ccfada6c8a58430a2e47417d727b
r33drichards <rwendt1337@gmail.com>
2024-03-25 12:50:21 -0700

    add turso cli, remove package thats not building

--- flake.nix
+++ flake.nix
@@ -17,30 +17,13 @@
               nodePackages.pnpm
               nodePackages.yarn
               nodejs
+              turso-cli
             ];
           };
-          # Example of how to use mkPnpmPackage to package a Node.js project using pnpm
-          app = pkgs.mkYarnPackage rec {
-            name = "app";
-            src = ./.;
-            buildPhase = ''
-                yarn --offline run build
-            '';
-
-            installPhase = ''
-              mkdir -p $out/dist
-              mv .output/* $out/dist
-            '';
-            NUXT_SESSION_PASSWORD="83e27ccd-b463-477a-a49f-8196184af1ac";
-            NUXT_UI_PRO_LICENSE="99D57DD6-BD0D-4E27-9924-D2306E9FDFBC";
-          };
-
-
 
         in
         {
           devShells.default = devshell;
-          packages.default = app;
         })
     );
 }
