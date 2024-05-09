d7456f354401c99eb9b581fe8fd64fffe023da56
rw <rw@jjk.is>
2024-05-09 16:45:20 -0700

    small fix for deployments

--- server/api/deployments.ts
+++ server/api/deployments.ts
@@ -8,19 +8,6 @@ export default eventHandler(async (event) => {
   const session = await requireUserSession(event)
   const userID = session.user.id
   let resp = await db.select().from(deployments).where(eq(deployments.userID, userID))
-  return await Promise.all(resp.map(async (deployment) => {
-    let r = await fetch(`${config.BACKEND_URL}/flake/${deployment.flakeComputeID}`, {
-      headers: {
-        "Content-Type": "application/json",
-        'X-Token': process.env.AUTH_TOKEN ?? "b355b95e-1933-4103-8f7e-156687fa0a1f",
-      },
-  })
-    let jsonResponse = await r.json()
-
-    return {
-      ...deployment,
-      ...jsonResponse,
-    }
-  }))
+  return resp
 
 })
