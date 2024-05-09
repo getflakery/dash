70ed4fb255d0c9f9b6f97485c52941414cf82454
rw <rw@jjk.is>
2024-05-08 22:21:59 -0700

    cssnano

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,6 +41,8 @@ export default defineNuxtConfig({
     enabled: true,
   },
   sourcemap: false,
+  postcss: { plugins: { tailwindcss: {}, autoprefixer: {}, cssnano: process.env.NODE_ENV === 'production' ? { preset: ['default', { discardComments: { removeAll: true } }] } : false, // disable cssnano when not in production 
+}, },
   // debug: true,
   // vite: {
   //   build: {
