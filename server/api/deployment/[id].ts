aaf6df46fad3637159b6ba3c28439a8dd9612f15
rw <rw@jjk.is>
2024-05-09 17:08:10 -0700

    remove old stuff

--- server/api/deployment/[id].ts
+++ server/api/deployment/[id].ts
@@ -17,30 +17,7 @@ export default eventHandler(async (event) => {
       eq(deployments.id, id))
   ).get();
 
-  let flakeComputeID = (await db.select().from(deployments).where(
-    and(
-      eq(deployments.id, id),
-    )
-  ).get())?.flakeComputeID
-
-  let r = await fetch(`${config.BACKEND_URL}/flake/${flakeComputeID}`, {
-    headers: {
-      "Content-Type": "application/json",
-      'X-Token': process.env.AUTH_TOKEN ?? "b355b95e-1933-4103-8f7e-156687fa0a1f",
-    },
-  })
-  let jsonResponse = await r.json()
-
-  const network = await db.select().from(networks).where(
-    and(
-      eq(networks.deploymentID, id),
-      eq(networks.userID, userID)
-    )
-  ).get()
-
   return {
     ...inst,
-    ...jsonResponse,
-    network: network ?? {},
   }
 })
