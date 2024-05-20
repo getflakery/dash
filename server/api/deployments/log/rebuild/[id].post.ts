2d05646c12ae601cbf3db91430d1f8f64fd68851
rw <rw@jjk.is>
2024-05-20 01:21:05 -0700

    no sess

--- server/api/deployments/log/rebuild/[id].post.ts
+++ server/api/deployments/log/rebuild/[id].post.ts
@@ -4,23 +4,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 export default eventHandler(async (event) => {
-
     const { id } = await useValidatedParams(event, {
         id: z.string().uuid(),
     })
     // log body
     let body = await readRawBody(event);
     let bodyJSON = JSON.parse(body);
-
     const db = useDB()
-    const session = await requireUserSession(event)
     await db.insert(deploymentLogs).values({
         id: uuidv4(),
         deploymentID: id,
         logs: bodyJSON,
     }).returning().get()
-
-    
-
-
 });
