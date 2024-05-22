cfa68a0967947468ade4ab019c803471b91bd6c3
rw <rw@jjk.is>
2024-05-22 00:16:57 -0700

    new ami id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
       public_subnet_2: 'subnet-061710e423b885a9a', // can be overridden by NUXT_PUBLIC_SUBNET_2 environment variable
       private_subnet_1: 'subnet-001d38fabf0cbc832', // can be overridden by NUXT_PRIVATE_SUBNET_1 environment variable
       private_subnet_2: 'subnet-0c5c6438c404fbf54', // can be overridden by NUXT_PRIVATE_SUBNET_2 environment variable
-      image_id: 'ami-0e9dc1ea24f0593ea', // can be overridden by NUXT_IMAGE_ID environment variable
+      image_id: 'ami-0cf12b6f7cdc16ba6', // can be overridden by NUXT_IMAGE_ID environment variable
     }
   },
 });
