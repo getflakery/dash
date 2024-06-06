52ad325f3f10719ddf5cefc5c2b328dec7cc0bfe
rw <rw@jjk.is>
2024-06-05 20:41:49 -0700

    dark mode

--- pages/dashboard/deployment/[id].vue
+++ pages/dashboard/deployment/[id].vue
@@ -2,7 +2,6 @@
 import type { Deployment, File } from '~/types'
 import { v4 as uuidv4 } from 'uuid';
 
-
 const route = useRoute()
 
 const { data: deployment, refresh } = await useFetch<Deployment>(`/api/deployment/${route.params.id}`)
@@ -11,17 +10,8 @@ const redeployModal = ref(false)
 
 const isOpen = ref(false)
 
-
-
 function getItems(deployment: Deployment, refresh: Function) {
   return [
-    // [{
-    //     label: 'Redeploy',
-    //     icon: 'i-heroicons-paper-airplane',
-    //     click: async () => {
-    //       redeployModal.value = true
-    //     }
-    //   }],  
     [{
       label: 'Delete',
       icon: 'i-heroicons-trash-20-solid',
@@ -29,22 +19,20 @@ function getItems(deployment: Deployment, refresh: Function) {
       click: async () => {
         deleteModal.value = true
       }
-    }]]
+    }]
+  ]
 }
-
 </script>
+
 <template>
   <UDashboardPage>
     <UDashboardPanel grow>
       <UDashboardNavbar :title="`Deployment Details for ${deployment?.name}`">
         <template #right>
           <UButton type="submit" label="Refresh" :onclick="refresh" icon="i-heroicons-arrow-path" />
-
           <UDropdown :items="getItems(i, refresh)" position="bottom-end">
             <UButton color="white" label="Actions" trailing-icon="i-heroicons-chevron-down-20-solid" />
           </UDropdown>
-
-
         </template>
       </UDashboardNavbar>
       <UDashboardPanelContent>
@@ -55,26 +43,21 @@ function getItems(deployment: Deployment, refresh: Function) {
           </NuxtLink>
         </UDashboardSection>
 
-
         <!-- Logs: -->
         <UDashboardSection title="Logs" orientation="horizontal" :ui="{ container: 'Flg:sticky top-2' }">
           <div class="code-block" style="position: relative; height: 300px;">
             <pre class="custom-scroll">
-                <code>
-                  {{ deployment?.logs?.reduce(
-                    (acc, log) => acc + log.exec + '\n',
-                    '') }}
-                </code>
-              </pre>
-              <UButton @click="isOpen = !isOpen" icon="i-heroicons-arrows-pointing-out" class="fullscreen-button" />
+              <code>
+                {{ deployment?.logs?.reduce((acc, log) => acc + log.exec + '\n', '') }}
+              </code>
+            </pre>
+            <UButton @click="isOpen = !isOpen" icon="i-heroicons-arrows-pointing-out" class="fullscreen-button" />
           </div>
-
         </UDashboardSection>
-
       </UDashboardPanelContent>
-
     </UDashboardPanel>
   </UDashboardPage>
+
   <UDashboardModal v-model="deleteModal" title="Confirm Delete" description="Delete Deployment"
     :ui="{ width: 'sm:max-w-md' }">
     <DeploymentsDeleteConfirmModal @close="deleteModal = false" :refresh="refresh" :deployment="deployment" />
@@ -87,9 +70,7 @@ function getItems(deployment: Deployment, refresh: Function) {
       base: 'h-full flex flex-col',
       rounded: '',
       divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-      body: {
-        base: 'grow'
-      }
+      body: { base: 'grow' }
     }">
       <template #header>
         <div class="flex items-center justify-between">
@@ -100,55 +81,46 @@ function getItems(deployment: Deployment, refresh: Function) {
             @click="isOpen = false" />
         </div>
       </template>
-
-
       <div class="code-block" style="position: relative;">
         <pre class="custom-scroll">
-                <code>
-                  {{ deployment?.logs?.reduce(
-                    (acc, log) => acc + log.exec + '\n',
-                    '') }}
-                </code>
-              </pre>
-
+          <code>
+            {{ deployment?.logs?.reduce((acc, log) => acc + log.exec + '\n', '') }}
+          </code>
+        </pre>
       </div>
     </UCard>
   </UModal>
 </template>
 
-
 <style scoped>
 /* Additional CSS for custom styling and scroll behavior */
 .code-block {
   overflow-x: auto;
-  /* Ensures that horizontal scrolling is only shown when necessary */
   background-color: #f5f5f5;
-  /* Light background color for the code block */
   border-radius: 4px;
-  /* Rounded corners for the code block */
   padding: 1rem;
-  /* Adequate padding around the content */
   font-family: 'Fira Code', monospace;
-  /* Monospace font for better code readability */
   overflow-x: scroll;
   position: relative;
+  color: #333; /* Default text color */
+}
+
+.dark .code-block {
+  background-color: #2d2d2d; /* Dark mode background color */
+  color: #ddd; /* Dark mode text color */
 }
 
 .custom-scroll::-webkit-scrollbar {
   width: 8px;
-  /* Custom scrollbar width */
 }
 
 .custom-scroll::-webkit-scrollbar-track {
   background: #e0e0e0;
-  /* Scrollbar track color */
 }
 
 .custom-scroll::-webkit-scrollbar-thumb {
   background-color: #a0a0a0;
-  /* Scrollbar thumb color */
   border-radius: 4px;
-  /* Rounded corners for the scrollbar thumb */
 }
 
 .fullscreen-button {
