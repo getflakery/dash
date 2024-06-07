cebb212be1ed53354574ccc536da3d8c959f7eff
rw <rw@jjk.is>
2024-06-07 14:03:39 -0700

    get logs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
       public_subnet_2: 'subnet-061710e423b885a9a', // can be overridden by NUXT_PUBLIC_SUBNET_2 environment variable
       private_subnet_1: 'subnet-001d38fabf0cbc832', // can be overridden by NUXT_PRIVATE_SUBNET_1 environment variable
       private_subnet_2: 'subnet-0c5c6438c404fbf54', // can be overridden by NUXT_PRIVATE_SUBNET_2 environment variable
-      image_id: 'ami-05bc736102184d8fa', // can be overridden by NUXT_IMAGE_ID environment variable
+      image_id: 'ami-0b44320ae20ca7e69', // can be overridden by NUXT_IMAGE_ID environment variable
     }
   },
 });
