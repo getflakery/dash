b1a07f55e165830a508addb58226a10245cf9e48
rw <rw@jjk.is>
2024-06-23 23:53:13 -0700

    cmd k deployments

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import type { Template } from '~/types'
+
+const { data: templates, refresh } = await useFetch<Template[]>('/api/templates', { default: () => [] })
+const isInviteModalOpen = ref(false)
+
 const route = useRoute()
 const appConfig = useAppConfig()
 const { isHelpSlideoverOpen } = useDashboard()
@@ -23,22 +28,27 @@ const footerLinks = [{
   click: () => isHelpSlideoverOpen.value = true
 }]
 
+const deployInstance = ref(false)
+const templateToDeploy = ref()
+
 const groups = [{
   key: 'links',
   label: 'Go to',
   commands: links.map(link => ({ ...link, shortcuts: link.tooltip?.shortcuts }))
 },
-{
-  key: 'create',
-  label: 'Create',
-  commands: [{
-    id: 'instance',
-    label: "Create Instance",
-    icon: 'i-heroicons-server',
-    click: () => createInstanceOpen.value = true
-  },
-  ]
-}
+  ...templates.value.map(template => ({
+    key: template.id,
+    label: template.name,
+    commands: [{
+      id: `deploy-${template.id}`,
+      label: `Deploy ${template.name} ${template.flakeURL}`,
+      icon: 'i-heroicons-server',
+      click: () => {
+        deployInstance.value = true
+        templateToDeploy.value = template
+      }
+    }]
+  }))
   //  {
   //   key: 'code',
   //   label: 'Code',
@@ -116,8 +126,10 @@ const colors = computed(() => defaultColors.value.map(color => ({ ...color, acti
       <LazyUDashboardSearch :groups="groups" />
     </ClientOnly>
   </UDashboardLayout>
-  <UDashboardModal v-model="createInstanceOpen" title="Create Instance" :ui="{ width: 'sm:max-w-md' }">
-    <!-- ~/components/settings/MembersForm.vue -->
-    <DeploymentsForm @close="createInstanceOpen = false" />
+
+  <UDashboardModal v-model="deployInstance"
+    :title="`Deploy Template: ${templateToDeploy?.name ? templateToDeploy?.name : templateToDeploy?.flakeURL}`"
+    description="" :ui="{ width: 'sm:max-w-md' }">
+    <TemplatesDeployModal @close="deployInstance = false" :refresh="refresh" :template="templateToDeploy" />
   </UDashboardModal>
 </template>
