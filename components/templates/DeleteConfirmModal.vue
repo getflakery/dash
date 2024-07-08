8b0b86ba5fb917498e1f2875f4097ee4a571ff9a
rw <rw@jjk.is>
2024-07-08 15:27:47 -0700

    add error messaging when trying to delete templates with active deployments

--- components/templates/DeleteConfirmModal.vue
+++ components/templates/DeleteConfirmModal.vue
@@ -11,17 +11,30 @@ const props = defineProps<{
 const emit = defineEmits(['close'])
 
 
+const err = ref(false)
+const errMessage = ref('')
+
 const confirmDelete = async (id: string, deleteFiles: boolean, deleteNetwork: boolean, refresh: () => void) => {
-  await $fetch(`/api/templates/delete/${id}`, {
-    method: 'POST',
-    body: JSON.stringify({
-      deleteFiles,
-      deleteNetwork,
+  console.log("hmm")
+
+  try {
+    await $fetch(`/api/templates/delete/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        deleteFiles,
+        deleteNetwork,
+      })
     })
-  })
-  refresh()
-  emit('close')
-  await navigateTo('/dashboard/templates')
+    refresh()
+    emit('close')
+    await navigateTo('/dashboard/templates')
+  } catch (e) {
+    console.log(e.data)
+    errMessage.value = e.data
+    err.value = true
+  }
+
+
 
 }
 
@@ -33,14 +46,31 @@ const deleteNetworks = ref(false)
 
 
 <template>
-  <div class="flex justify-end gap-3">
-    <UButton label="Cancel" color="gray" variant="ghost" @click="emit('close')" />
-    <UButton type="submit" label="Confirm Delete" color="black" @click="confirmDelete(
-      props.template.id,
-      deleteFiles,
-      deleteNetworks,
-      props.refresh
-    )" />
+  <!-- if err display error message, otherwise display regular modal -->
+
+  <div v-if="err" >
+    <div>
+      <pre class="text-red-500 dark:text-red-400 whitespace-pre-line">{{ errMessage }}</pre>
+
+    </div>
+    <div class="flex justify-end gap-3"> 
+      <UButton label="Close" color="gray" variant="ghost" @click="emit('close')"/>
+
+    </div>
+
+
+
+  </div>
+  <div v-else>
+    <div class="flex justify-end gap-3">
+      <UButton label="Cancel" color="gray" variant="ghost" @click="emit('close')" />
+      <UButton type="submit" label="Confirm Delete" color="black" @click="confirmDelete(
+        props.template.id,
+        deleteFiles,
+        deleteNetworks,
+        props.refresh
+      )" />
+    </div>
   </div>
 </template>
 
--- server/api/templates/delete/[id].post.ts
+++ server/api/templates/delete/[id].post.ts
@@ -1,42 +1,41 @@
 import { useValidatedParams, useValidatedBody, z, } from 'h3-zod'
-import { templates, templateFiles as schemaTemplateFiles, files, networks} from '~/server/database/schema'
+import { templates, templateFiles as schemaTemplateFiles, files, deployments } from '~/server/database/schema'
 import { eq, and } from 'drizzle-orm'
 
 export default eventHandler(async (event) => {
   const { id } = await useValidatedParams(event, {
     id: z.string().uuid(),
   })
-  const {
-    deleteFiles,
-    deleteNetwork
-  } = await useValidatedBody(event, {
-    deleteFiles: z.boolean(),
-    deleteNetwork: z.boolean()
-  })
 
   const session = await requireUserSession(event)
   const userID = session.user.id
   const db = useDB()
 
-  if (deleteFiles) {
-    // delete all files associated with the template through templateFiles
-    // if the file has no other associations 
-    // then delete the file from the files table
-    const associatedFiles = await db.select().from(schemaTemplateFiles).where(eq(schemaTemplateFiles.templateId, id)).all()
-    associatedFiles?.forEach(async (file) => {
-      const otherAssociations = await db.select().from(schemaTemplateFiles).where(and(eq(schemaTemplateFiles.fileId, file.fileId), eq(schemaTemplateFiles.templateId, id))).get()
-      if (otherAssociations !== undefined) {
-        await db.delete(schemaTemplateFiles).where(eq(schemaTemplateFiles.fileId, file.fileId)).execute()
-        await db.delete(files).where(eq(files.id, file.fileId)).execute()
-      } else {
-        await db.delete(schemaTemplateFiles).where(and(eq(schemaTemplateFiles.fileId, file.fileId), eq(schemaTemplateFiles.templateId, id))).execute()
-      }
-    })
+  // if template has deployments, return 400 error 
+  const deps = await db.select().from(deployments).where(eq(deployments.templateID, id)).all()
 
-
-  } else {
-    // remove the association from templateFiles but do not delete the files
-    await db.delete(schemaTemplateFiles).where(eq(schemaTemplateFiles.templateId, id)).execute()
+  if (deps.length > 0) {
+    let msg = `Cannot delete template with active deployments: \n\n ${deps.map((dep) => dep.name).join('\n')}
+    
+    please delete deployments first.`
+    return new Response(msg, { status: 400 })
   }
+
+
+  // delete all files associated with the template through templateFiles
+  // if the file has no other associations 
+  // then delete the file from the files table
+  const associatedFiles = await db.select().from(schemaTemplateFiles).where(eq(schemaTemplateFiles.templateId, id)).all()
+  associatedFiles?.forEach(async (file) => {
+    const otherAssociations = await db.select().from(schemaTemplateFiles).where(and(eq(schemaTemplateFiles.fileId, file.fileId), eq(schemaTemplateFiles.templateId, id))).get()
+    if (otherAssociations !== undefined) {
+      await db.delete(schemaTemplateFiles).where(eq(schemaTemplateFiles.fileId, file.fileId)).execute()
+      await db.delete(files).where(eq(files.id, file.fileId)).execute()
+    } else {
+      await db.delete(schemaTemplateFiles).where(and(eq(schemaTemplateFiles.fileId, file.fileId), eq(schemaTemplateFiles.templateId, id))).execute()
+    }
+  })
+
+
   return db.delete(templates).where(eq(templates.id, id))
 })
