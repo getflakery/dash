e99415f30aa16a7705eeb1648a54590b46dd11ff
rw <rw@jjk.is>
2024-07-10 13:42:30 -0700

    parse jwt

--- server/middleware/tokenAuth.ts
+++ server/middleware/tokenAuth.ts
@@ -3,6 +3,9 @@ import jwt from 'jsonwebtoken';
 export default defineEventHandler(async (event) => {
     // if path start with /api/v0, check for authentication header
     if (event.path.startsWith('/api/v0')) {
-        event.context.auth =  await jwt.verify(event.req.headers.authentication, 'mysecrettoken');
+        let tokenHeader = event.headers.get('Authorization');
+        let token = tokenHeader?.split(' ')[1];
+        let config = useRuntimeConfig();
+        event.context.auth =  await jwt.verify(token, config.jwt_secret);
     }
 });
