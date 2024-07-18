a9f7f48b53e00ac88eb72d2f5b96931ef4a2be95
rw <rw@jjk.is>
2024-07-18 15:13:38 -0700

    wild guess

--- server/api/templates.post.ts
+++ server/api/templates.post.ts
@@ -168,7 +168,7 @@ export default eventHandler(async (event) => {
 
   // check if privateBinaryCache exists for this user
   // if not, create one
-  const existingPrivateBinaryCache = await db.select().from(privateBinaryCache).where(eq(privateBinaryCache.name, userID)).get();
+  const existingPrivateBinaryCache = await db.select().from(privateBinaryCache).where(eq(privateBinaryCache.name, userID.toString())).get();
   console.log('existingPrivateBinaryCache', existingPrivateBinaryCache)
   if (!existingPrivateBinaryCache) {
     const bcacheID = "9177d3f8-0300-4946-955d-d23c1de83d8f"; // todo hardcoded tech debt
