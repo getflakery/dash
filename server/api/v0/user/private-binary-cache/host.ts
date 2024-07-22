f3cbe6b7fe686a362fdbada58eef35d9f7761371
rw <rw@jjk.is>
2024-07-22 13:18:55 -0700

    get pub key and host

--- server/api/v0/user/private-binary-cache/host.ts
+++ server/api/v0/user/private-binary-cache/host.ts
@@ -3,34 +3,25 @@ import { useValidatedParams, z } from "h3-zod"
 import { deployments, privateBinaryCache, target } from "~/server/database/schema"
 
 export default eventHandler(async (event) => {
-    console.log('get id')
-    const { id } = await useValidatedParams(event, {
-        id: z.string(),
-    })
+    let id = event.context.auth?.userID.toString()
+
+
+    if (!id.includes('.0')) {
+        id = id + '.0'
+    }
+
+    console.log('get id')   
+    console.log(id)
 
     const db = useDB()
 
-    const { host } = await db.select({
-        host: target.host,
-    }).from(privateBinaryCache).where(
+    const { publickey } = await db.select().from(privateBinaryCache).where(
         eq(privateBinaryCache.name, id)
-    ).innerJoin(deployments, eq(deployments.id, privateBinaryCache.deploymentID))
-        .innerJoin(target, eq(target.deploymentID, deployments.id))
-        .limit(1)
+    ).get()
 
-    if (host == null || host == undefined) {
+    if (publickey == null || publickey == undefined) {
         return new Response('not found', { status: 404 })
     }
 
-    const { pubkey } = await db.select({
-        pubkey: privateBinaryCache.publickey,
-    }).from(privateBinaryCache).where(
-        eq(privateBinaryCache.name, id)
-    ).limit(1)
-
-    if (pubkey == null || pubkey == undefined) {
-        return new Response('not found', { status: 404 })
-    }
-
-    return { host, pubkey }
+    return {  publickey, host: publickey.split(':')[0] }
 })
