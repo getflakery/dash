e6055f7c6a65de952512d3bfc158339e80cc6a31
rw <rw@jjk.is>
2024-08-13 22:31:43 -0700

    push

--- terraform/state/main.tf
+++ terraform/state/main.tf
@@ -31,6 +31,7 @@ resource "aws_iam_policy" "github_actions" {
         Action   = [
             "ecr:*",
             "ecr-public:*",
+            "sts:GetServiceBearerToken"
         ]
         Resource = "*"
       },
