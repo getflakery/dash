01b7ee37795f5329196c609852af697fb6fb6384
rw <rw@jjk.is>
2024-08-13 23:41:34 -0700

    maybe its lost

--- envs.sh
+++ envs.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p nodePackages.vercel 
-
-# Replace with your Vercel API token
-# VERCEL_TOKEN=
-
-# Replace with your Vercel project ID
-# VERCEL_PROJECT_ID=
-
-# Replace with your Vercel project ID
-# Fetch environment variables from Vercel
 rm -f .env
-vercel env pull --environment=production  --token $VERCEL_TOKEN .env
+
+vercel env pull --environment=production --token $VERCEL_TOKEN .env
