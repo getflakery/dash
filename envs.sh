297c61a49ed39a5614375b72296cb52f9345fe07
rw <rw@jjk.is>
2024-08-13 23:33:15 -0700

    prd

--- envs.sh
+++ envs.sh
@@ -10,4 +10,4 @@
 # Replace with your Vercel project ID
 # Fetch environment variables from Vercel
 rm -f .env
-vercel env pull  --token $VERCEL_TOKEN .env
+vercel env pull --environment=production  --token $VERCEL_TOKEN .env
