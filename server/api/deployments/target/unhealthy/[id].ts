5b017a61ed4688d8173fc8e00c765a96a78a973f
rw <rw@jjk.is>
2024-08-14 18:27:03 -0700

    log

--- server/api/deployments/target/unhealthy/[id].ts
+++ server/api/deployments/target/unhealthy/[id].ts
@@ -4,21 +4,28 @@ import { eq, and } from 'drizzle-orm'
 import { target as targetSchema } from '~/server/database/schema'
 
 export default eventHandler(async (event) => {
+    console.log("unhealthy target")
     const { target } = await useValidatedBody(event, {
         target: z.string(),
     })
+    console.log("target", target)
 
     const id = await useValidatedParams(event, {
         id: z.string(),
     })
+
+    console.log("id", id)
     const db = useDB()
     //   update the target to be unhealthy
-    return await db.update(targetSchema).set({
+    const resp = await db.update(targetSchema).set({
         healthy: 0,
     }).where(
         and(
             eq(targetSchema.deploymentID, id),
             eq(targetSchema.host, target),
         )
-    ).get()
+    )
+    console.log("resp", resp)
+    return resp
+    
 })
