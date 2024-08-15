a371e623b314b05b20c8fa9e0744fb7decf12e6d
rw <rw@jjk.is>
2024-08-14 17:20:52 -0700

    wtf?

--- server/api/deployments/lb-config-ng.ts
+++ server/api/deployments/lb-config-ng.ts
@@ -1,5 +1,6 @@
 import { deployments, target, templates } from "~/server/database/schema";
 import { inArray, eq } from 'drizzle-orm'
+import { createClient } from "@libsql/client";
 
 
 
@@ -21,6 +22,19 @@ interface services {
 
 export default eventHandler(async (event) => {
     console.log("lb config");
+
+    const config = useRuntimeConfig()
+
+     const turso = createClient({
+        url: config.db_url,
+        authToken: config.turso_token,
+      });
+
+    //   seleect 1+1 to test
+      const resp = await turso.execute("SELECT 1+1");
+      console.log(resp)
+
+      
 
     const db = useDB()
 
